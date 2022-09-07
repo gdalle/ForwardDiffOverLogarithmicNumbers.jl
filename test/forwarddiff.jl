0d7fe8d7bcd23872a7afd9e2466d7c1c54509c6d
longemen3000 <longemen3000@gmail.com>
2022-09-07 13:42:40 -0400

    add constructors

--- test/forwarddiff.jl
+++ test/forwarddiff.jl
@@ -4,6 +4,8 @@ using ForwardDiffOverLogarithmicNumbers
 
 f(x) = exp(x)
 g(x) = exp(Logarithmic, x)
+h(x) = exp(ULogarithmic,x)
 
 @test ForwardDiff.derivative(f, 1.) ≈ exp(1)
-@test_broken ForwardDiff.derivative(g, 1.) ≈ exp(1)
+@test ForwardDiff.derivative(g, 1.) ≈ exp(1)
+@test ForwardDiff.derivative(h, 1.) ≈ exp(1)
