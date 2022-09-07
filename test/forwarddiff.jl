53551271b83458acc247fc9f441151f76adb5167
longemen3000 <longemen3000@gmail.com>
2022-09-07 13:47:42 -0400

    modify test values to use very large numbers

--- test/forwarddiff.jl
+++ test/forwarddiff.jl
@@ -7,5 +7,6 @@ g(x) = exp(Logarithmic, x)
 h(x) = exp(ULogarithmic,x)
 
 @test ForwardDiff.derivative(f, 1.) ≈ exp(1)
-@test ForwardDiff.derivative(g, 1.) ≈ exp(1)
-@test ForwardDiff.derivative(h, 1.) ≈ exp(1)
+#ideally, test over ranges where any unwanted conversion would overflow
+@test ForwardDiff.derivative(g, 1000.0) ≈ exp(Logarithmic,1000.0)
+@test ForwardDiff.derivative(h, 1000.0) ≈ exp(ULogarithmic,1000.0)
